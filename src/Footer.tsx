7a46cfd4bd58b6438cf8e60b968cf2c562d73eb5
Lilit <151066726+bareghamyanlilit@users.noreply.github.com>
2025-09-27 01:01:43 +0400

    .

---
+++ src/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>
+        <p>
+          &copy; Copyright by
+          <a target="_blank" href="#">Lilit</a> .
+        </p>
+        <p><span className="u-bold u-italic">Not allowed</span> use this</p>
+      </small>
+    </footer>
+  );
+}
